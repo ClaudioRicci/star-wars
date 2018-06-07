37bbacf2716ae6ad7521ba9aebcae3f7a0b99cdc
CLAUDIO RICCI <claudioricci1@hotmail.com>
2018-06-08 00:41:31 +0100

    initial-commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { PanelRoutes } from './Components/Panels/PanelRoutes';
+import { Title } from './Components/Title/Title';
+import { GameRow } from './Components/GameRow/GameRow';
+import { PlayerOne } from './Components/PlayerOne/PlayerOne';
+import { PlayerTwo } from './Components/PlayerTwo/PlayerTwo';
+import { FinalScore } from './Components/FinalScore/FinalScore';
+import { Sets } from './Components/Sets/Sets';
+import { DeleteButton } from './Components/DeleteButton/DeleteButton';
+import { MobileView } from './Components/MobileView/MobileView';
+
+export {
+    PlayerOne,
+    PlayerTwo,
+    FinalScore,
+    Sets,
+    DeleteButton,
+    MobileView
+}
 
 import './main.scss';
 
-const title = 'Claudio Ricci Star Wars';
+const title = 'Claudio Ricci Table Tennis Scoreboard';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <PanelRoutes/>
+      <div className="gameSurround">
+      	<Title/>
+      	<GameRow/>
       </div>
     );
   }
